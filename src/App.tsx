dfbf90b8b565d3a95d3a82e3067bfe961da0867c
Himanshu Jain <jainh246@gmail.com>
2023-10-29 01:08:06 +0530

    feat: add box geometry

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+//components
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+
 function App() {
-  return <div className="h-screen w-screen bg-amber-400"></div>;
+  return (
+    <div className="h-screen w-screen bg-amber-400">
+      <Canvas>
+        <Experience />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
